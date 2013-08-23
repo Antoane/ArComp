a3f4a3c518d8fded3560dad0f675a490ee79b819
hollaus.christian@gmail.com <hollaus.christian@gmail.com@6960c391-9e2b-1ca0-1ec9-f95c5cc3a501>
2013-08-23 15:33:05 +0000

    Distanzen-Dialog fertiggestellt

---  arcomp2 --username hollaus.christian@gmail.com/pas/DAL/ARC_DAL_Tools.pas
+++  arcomp2 --username hollaus.christian@gmail.com/pas/DAL/ARC_DAL_Tools.pas
@@ -4,12 +4,15 @@ interface
 
 uses
   Data.Win.ADODB,
+  sysutils,
 
   //ArComp
   ARC_Types;
 
 type
   TARC_DAL_Tools = class(TObject)
+  private
+    class procedure datenbankBereinigen(connection: TADOConnection); static;
 
   public
     class function getIdForName(const name: string; const typ: TFieldType; connection: TADOConnection): string;
@@ -68,6 +71,43 @@ begin
       begin
         result := aQuery.Fields[0].AsString;
       end;
+    end;
+  finally
+    aQuery.Free;
+  end;
+end;
+
+class procedure TARC_DAL_Tools.datenbankBereinigen(connection: TADOConnection);
+var
+  aQuery: TADOQuery;
+begin
+  aQuery := TADOQuery.Create(nil);
+  try
+    aQuery.connection := connection;
+    with aQuery.SQL do
+    begin
+      clear;
+      add('DELETE FROM ALTERSKATEGORIE');
+      add('WHERE isNull(AK_NAME,' + quotedstr('') + ') = ' + quotedstr(''));
+      add('');
+      add('DELETE FROM BOGENKATEGORIE');
+      add('WHERE isNull(BK_NAME,' + quotedstr('') + ') = ' + quotedstr(''));
+      add('');
+      add('DELETE FROM GESCHLECHT');
+      add('WHERE isNull(GE_NAME,' + quotedstr('') + ') = ' + quotedstr(''));
+      add('');
+      add('DELETE FROM PERSON');
+      add('WHERE isNull(PE_VORNAME,' + quotedstr('') + ') = ' + quotedstr(''));
+      add('  AND isNull(PE_NACHNAME,' + quotedstr('') + ') = ' + quotedstr(''));
+      add('');
+      add('DELETE FROM TURNIER');
+      add('WHERE isNull(TU_NAME,' + quotedstr('') + ') = ' + quotedstr(''));
+      add('');
+      add('DELETE FROM TURNIERART');
+      add('WHERE isNull(TA_NAME,' + quotedstr('') + ') = ' + quotedstr(''));
+      add('');
+      add('DELETE FROM VEREIN');
+      add('WHERE isNull(VE_NAME,' + quotedstr('') + ') = ' + quotedstr(''));
     end;
   finally
     aQuery.Free;
